fa62eee2b3afd7eae110d2560ad123f8a92ab00d
Enzo Mello <etmello@sp.r7.com>
2015-12-04 20:42:31 -0200

    trying to do a scrollTo without jquery

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,26 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+window.Helpers = {};
+
+Helpers.addEvent = function(eventName, elementClass, callback) {
+  var elementsArray = document.querySelectorAll('.' + elementClass);
+
+  for (var i = 0; i < elementsArray.length; i++) {
+    elementsArray[i].addEventListener(eventName, callback, false);
+  }
+};
+
+window.App = {};
+
+App.scrollToAnchor = function(params) {
+  var targetHref = params.target.href.split('/'),
+      anchorId = targetHref[targetHref.length - 1].replace('#', ''),
+      anchorPosition = document.getElementById(anchorId).offsetTop;
+
+  event.preventDefault();
+
+  window.scrollTo(0, anchorPosition);
+}
+
+App.init = function() {
+  Helpers.addEvent('click', 'scrollable-link', this.scrollToAnchor);
+};
