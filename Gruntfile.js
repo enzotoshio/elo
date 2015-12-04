0cd9928c30e2d2897c5edf149d97be29e6fa0bc3
Enzo Mello <etmello@sp.r7.com>
2015-12-04 20:42:59 -0200

    making grunt imagemin run on dev mode

--- Gruntfile.js
+++ Gruntfile.js
@@ -75,7 +75,7 @@ module.exports = function (grunt) {
         open: true,
         livereload: 35729,
         // Change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost'
+        hostname: '0.0.0.0'
       },
       livereload: {
         options: {
@@ -342,7 +342,8 @@ module.exports = function (grunt) {
     concurrent: {
       server: [
         'sass:server',
-        'copy:styles'
+        'copy:styles',
+        'imagemin'
       ],
       test: [
         'copy:styles'
