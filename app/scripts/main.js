5eba74730f9bf5946c3c8df7080c3d53f93e7a09
Enzo Toshio <enzo.toshiba@gmail.com>
2015-12-05 23:11:23 -0200

    finishing js to do the page scrolling and adding modernizr to check some supports

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,11 +14,34 @@ window.App = {};
 App.scrollToAnchor = function(params) {
   var targetHref = params.target.href.split('/'),
       anchorId = targetHref[targetHref.length - 1].replace('#', ''),
-      anchorPosition = document.getElementById(anchorId).offsetTop;
+      anchorPosition = document.getElementById(anchorId).offsetTop,
+      bodyEl = document.body,
+      transitionDuration = 500,
+      visibleScreen = window.innerHeight,
+      positionToGo = anchorPosition - window.pageYOffset,
+      screenEnding = document.documentElement.scrollHeight - visibleScreen,
+      hasTransformAndTransition = Modernizr.testAllProps('transform') &&
+        Modernizr.testAllProps('transition');
+
+  if (!hasTransformAndTransition) {
+    return;  
+  }
 
   event.preventDefault();
 
-  window.scrollTo(0, anchorPosition);
+  if(anchorPosition >= screenEnding) {
+    positionToGo = screenEnding - window.pageYOffset;
+  }
+
+  bodyEl.style.transition = 'transform '+ transitionDuration + 'ms ease';
+  bodyEl.style.WebkitTransform = 'translate(0, -' + positionToGo + 'px)';
+  bodyEl.style.MozTransform = 'translate(0, -' + positionToGo + 'px)';
+  bodyEl.style.transform = 'translate(0, -' + positionToGo + 'px)';
+
+  setTimeout(function() {
+    bodyEl.removeAttribute('style');
+    window.scrollTo(0, anchorPosition);
+  }, transitionDuration);
 }
 
 App.init = function() {
